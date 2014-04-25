3d8fdaa057ce79245dcfbaa55df2f01ba0a7bc08
z0m <dmg.vss@gmail.com>
2014-04-25 22:31:54 +0100

    Added guess count

--- guess.py
+++ guess.py
@@ -4,13 +4,18 @@
 import random
 number = random.randint(1,20)
 guess = -1
+count = 0
 
 while (guess != number):
 	guess = int(input("Guess the number: "))
-
+	count = count + 1
 	if guess == number:
 		print("You got it!")
 	elif guess < number:
 		print("The number is bigger than that.")
 	elif guess > number:
 		print("The number is smaller than that.")
+if count > 3:
+	print("That must have been tough.")
+else:
+	print("That was quick!")
